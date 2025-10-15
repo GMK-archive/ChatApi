3d62b9d8752b7e6ddf7284bc1548c400b06ced54
4189594 <4189594@TE-GDA-D-D21E06>
2025-10-15 13:09:43 +0200

    zadanie

--- WebApplication9/Program.cs
+++ WebApplication9/Program.cs
@@ -1,3 +1,4 @@
+using System.Web;
 using WebApplication9;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,12 +10,18 @@ chatHistory.AddMessage(new ChatMessage("User2", "Deez Nuts"));
 chatHistory.AddMessage(new ChatMessage("User3", "67"));
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/chat", (DateTime? timestamp) =>
+app.MapGet("/chat", (string? timestamp) =>
 {
     if (timestamp == null)
         return chatHistory.GetLast(10);
-
-    return chatHistory.GetMessagesAfter(timestamp ?? DateTime.Now);
+    DateTime parsedTimestamp = DateTime.Now;
+    DateTime.TryParse(timestamp, out parsedTimestamp);
+    return chatHistory.GetMessagesAfter(parsedTimestamp);
+});
+app.MapPost("/chat", (ChatMessage message) =>
+{
+    chatHistory.AddMessage(message);
+    return Results.Created($"/chat/{message.Timestamp.Ticks}", message);
 });
 
 app.Run();
