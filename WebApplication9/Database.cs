7d270f5db340c5d1305c61a9493e268ff98ad99b
4189594 <4189594@TE-GDA-D-D21E05>
2025-10-16 13:28:58 +0200

    updated database

---
+++ WebApplication9/Database.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebApplication9
+{
+    public class Database : DbContext
+    {
+        public DbSet<ChatMessage> Messages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=chat.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChatMessage>().ToTable("Messages");
+            modelBuilder.Entity<ChatMessage>().HasKey(m => m.Timestamp);
+            modelBuilder.Entity<ChatMessage>().Property(m => m.Autor).IsRequired();
+            modelBuilder.Entity<ChatMessage>().Property(m => m.Content).IsRequired();
+        }
+    }
+}
